de4049420bb565f2820bc6704c53bf6372a11024
Mohammadreza Hooshmand <whyz.nasiri.85@gmail.com>
2025-09-16 19:20:04 +0330

    operator in arrays

--- s1/numpy/main.py
+++ s1/numpy/main.py
@@ -11,9 +11,23 @@ print(arr.ndim)
 print(arr.dtype)
 print(f"size= {arr.size}")
 print(f"new array= {NP.arange(0, 100, 3.2, "float32")}")
-print(f"new array= {NP.linspace(0, 100, 5,dtype=int)}")
-print(f"array ones=\n {NP.ones(shape=(3,3))}")
-print(f"array zeros=\n {NP.zeros(shape=(3,3))}")
-print(f"array full=\n {NP.full((3,3),66)}")
-print(f"array random=\n {(NP.random.rand(3,3)*10).round()}")
+print(f"new array= {NP.linspace(0, 100, 5, dtype=int)}")
+print(f"array ones=\n {NP.ones(shape=(3, 3))}")
+print(f"array zeros=\n {NP.zeros(shape=(3, 3))}")
+print(f"array full=\n {NP.full((3, 3), 66)}")
+print(f"array random=\n {(NP.random.rand(3, 3)*10).round()}")
 print(arr.shape)
+
+# operator in arrays
+
+array1 = NP.array([[1, 3], [4, 5]], dtype="float32")
+array2 = NP.array([[3, 2], [7, 8]], dtype="float32")
+
+print(f"sum array= \n {NP.add(array1,array2)}")
+print(f"multi array= \n {NP.multiply(array1,array2)}")
+print(f"division array= \n {NP.divide(array1,array2)}")
+
+print(array2.flatten())
+print(array2.ravel())
+
+print(f"concat array=\n {NP.concat([array1,array2])}")
