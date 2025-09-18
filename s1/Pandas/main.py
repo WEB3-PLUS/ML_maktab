c4ea5a0c6a28cb4cc704d85391bca8c131daae40
Mohammadreza Hooshmand <whyz.nasiri.85@gmail.com>
2025-09-18 22:11:44 +0330

    pandas 3

--- s1/Pandas/main.py
+++ s1/Pandas/main.py
@@ -4,9 +4,18 @@ titanic_data=pd.read_csv("./titacnic-dataset.csv")
 
 # print(titanic_data)
 
-print(titanic_data.head(5))
-print(titanic_data.tail(5))
-print(titanic_data.shape)
+# print(titanic_data.head(5))
+# print(titanic_data.tail(5))
+# print(titanic_data.shape)
+# print(titanic_data.columns)
+# print(titanic_data.dtypes)
+# print(titanic_data.info() )
+
+
 print(titanic_data.columns)
-print(titanic_data.dtypes)
-print(titanic_data.info() )
+print(titanic_data[["Survived","Age"]][0:10])
+# print(f"count of dead: {(titanic_data["Survived"].value_counts()).__getitem__(["0"])}")
+
+print(titanic_data["Survived"].value_counts(normalize=True)*100 )
+print(pd.crosstab(titanic_data.Sex,titanic_data.Survived))
+print(pd.crosstab(titanic_data.Sex,titanic_data.Survived,normalize="columns"))
