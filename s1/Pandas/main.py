9b7697e60f1cf9254a07881dde386212f1389eec
Mohammadreza Hooshmand <whyz.nasiri.85@gmail.com>
2025-09-20 15:25:02 +0330

    create group and sorting

--- s1/Pandas/main.py
+++ s1/Pandas/main.py
@@ -19,3 +19,7 @@ print(titanic_data[["Survived","Age"]][0:10])
 print(titanic_data["Survived"].value_counts(normalize=True)*100 )
 print(pd.crosstab(titanic_data.Sex,titanic_data.Survived))
 print(pd.crosstab(titanic_data.Sex,titanic_data.Survived,normalize="columns"))
+
+print(titanic_data[titanic_data["Age"] <=5]["Survived"].value_counts(normalize=True)*100)
+print(titanic_data["Name"].str.contains("Allen").value_counts())
+print(titanic_data.groupby(["Sex","Pclass"])["Fare"].mean().sort_values(ascending=False))
