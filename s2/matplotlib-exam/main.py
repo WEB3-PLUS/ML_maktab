1837e8a522e957f29ea97a26f4534a702e83e0b2
Mohammadreza Hooshmand <whyz.nasiri.85@gmail.com>
2025-09-26 12:06:02 +0330

    create bar chart

--- s2/matplotlib-exam/main.py
+++ s2/matplotlib-exam/main.py
@@ -3,16 +3,30 @@ import numpy as np
 
 # simple chart
 
-x = [13, 9, 0, 22, 1]
-y = [7, 23, 14, 19, 3]
-y2 = [10, 2, 40, 9, 31]
+# x = [13, 9, 0, 22, 1]
+# y = [7, 23, 14, 19, 3]
+# y2 = [10, 2, 40, 9, 31]
 
 
-plt.plot(x, y, color="skyblue", linestyle="--", marker="*")
-plt.plot(x, y2, color="purple", linestyle="--", marker="*")
-plt.xlabel("x axis", color='red')
-plt.ylabel("y axis", color="green")
+# plt.plot(x, y, color="skyblue", linestyle="--", marker="*")
+# plt.plot(x, y2, color="purple", linestyle="--", marker="*")
+# plt.xlabel("x axis", color='red')
+# plt.ylabel("y axis", color="green")
 
 
-plt.legend()
+# plt.legend()
+# plt.show()
+
+
+# bar chart
+
+langs = ["c#", "javascript", "c++", "java"]
+pop = [10, 70, 100, 45]
+
+plt.bar(langs, pop, color="purple")
+
+plt.xlabel("pop")
+plt.ylabel("langs")
+plt.title("chart of pop langs")
+
 plt.show()
