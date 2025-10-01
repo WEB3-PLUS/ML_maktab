87d1622c8bc660cd85cb1beff2809463ae4a336f
Mohammadreza Hooshmand <whyz.nasiri.85@gmail.com>
2025-10-01 12:33:47 +0330

    compeleted

--- s2/google-play-exam/main.py
+++ s2/google-play-exam/main.py
@@ -18,8 +18,18 @@ print(f"\n\n{data_frame.describe()}")
 print(f"missing value=\n")
 print(data_frame.isnull().sum())
 
+# data_frame=np.array(data_frame)
 
+plt.pie(x=data_frame["Type"].value_counts().values[:2],labels=data_frame["Type"].value_counts().keys()[:2],autopct='%1.1f%%')
+plt.title("paid/free")
+plt.show()
 
+topAppsInstallByCategory=data_frame[data_frame["Rating"]>4.9]["Category"].value_counts(normalize=True)
 
+scs.barplot(x=(topAppsInstallByCategory.values*100)[:11],y=topAppsInstallByCategory.keys()[:11])
+plt.title="top 10 apps rating: "
+plt.grid(True)
+
+plt.show()
 
 # scs.barplot(data=data_frame)
